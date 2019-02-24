b2f74db9a6daeb071ad1a9798125b472a2ff6b77
Arman Singhal <42670687+Tprolifer@users.noreply.github.com>
2019-02-24 12:01:53 +0530

    Update Bidder.sol

--- Bidder.sol
+++ Bidder.sol
@@ -1,24 +1,24 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.0; //Defines version of Solidity
 
-contract bidder{
-    string public name;
+contract bidder{    //Contract bidder initialised
+    string public name;                              //State Variables defined
     uint public bidAmount = 20000;
     bool public eligible;
-    uint constant minBid = 1000;
+    uint constant minBid = 1000;                    //Constant State Vaiable (sets minimum bid to 1000)
     
-    function setName(string nam) public{
+    function setName(string nam) public{           //Sets the bidder name
         name = nam;
     }
     
-    function setBidderAmount(uint amt) public{
+    function setBidderAmount(uint amt) public{    //Sets Bidder amount
         bidAmount = amt;
     }
     
-    function determineEligibilty() public{
-        if(bidAmount >= minBid){
-            eligible = true;
+    function determineEligibilty() public{       //This will determine bidder's eligibility to bid
+        if(bidAmount >= minBid){                 //Condition for eligibility
+            eligible = true;                     //If eligible then set eligibility to true
         }else{
-            eligible = false;
+            eligible = false;                    //If not eligible then set eligibility to false
         }
     }
 }
