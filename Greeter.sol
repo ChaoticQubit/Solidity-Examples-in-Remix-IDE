b1e23d35e71debae31a0db2c419cfb052dc0e3d0
Arman Singhal <42670687+Tprolifer@users.noreply.github.com>
2019-02-24 11:56:32 +0530

    Update Greeter.sol

--- Greeter.sol
+++ Greeter.sol
@@ -1,18 +1,18 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.0; //Defines the version of solidity
 
-contract greeter{
-    string public Name;
-    string public hellokey = "Hello ";
+contract greeter{       //Contract Greeter is defined
+    string public Name;                  //Data Variable or State variable defined as public 
+    string public hellokey = "Hello ";   //Data Variable or State variable assigned a value
     
-    function greeter() public{
+    function greeter() public{           //Function greeter is a constructor funtion
         Name = "Arman";
     }
     
-    function set(string greet) public {
+    function set(string greet) public {   //Function set will set the value in greet in the name variable
         Name = greet;
     }
     
-    function hello() constant public returns (string, string){
+    function hello() constant public returns (string, string){    //This will return "Hello Arman"
         return (hellokey, Name);
        
     }
