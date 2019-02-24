aed32b2f7e57e25543b3175b931fdec50db3c395
Arman Singhal <42670687+Tprolifer@users.noreply.github.com>
2019-02-24 12:06:22 +0530

    Update Simple_Calculator.sol

--- Simple_Calculator.sol
+++ Simple_Calculator.sol
@@ -1,29 +1,29 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.0;   //Defines version of Solidity
 
-contract operations{
-    uint value;
+contract operations{      //Contract operations initialised
+    uint value;           //State Variable
     
-    function set(uint x) public{
+    function set(uint x) public{    //Sets user value to value data variable
         value = x;
     }
     
-    function get() public constant returns(uint){
+    function get() public constant returns(uint){     //gets the data inside value variable
         return value;
     }
     
-    function incre(uint n) public{
+    function incre(uint n) public{           //Increments value by a specific no given by the user
         value += n;
     }
     
-    function decre(uint n) public{
+    function decre(uint n) public{          //Decrements value by a specific no given by the user
         value -= n;
     }
     
-    function multi(uint n) public{
+    function multi(uint n) public{          //Multiplies value by a specific no given by the user
         value = value * n;
     }
     
-    function divi(uint n) public{
+    function divi(uint n) public{          //Divides value by a specific no given by the user
         value = value / n;
     }
 }
